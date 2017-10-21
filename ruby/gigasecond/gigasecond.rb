8f032b14edf20d0f512cd7ae2f978e3b8f16130b
rlgreen91 <rlgreen91@gmail.com>
2017-10-21 17:55:01 -0500

    Updates gigasecond.rb to freeze constants.

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,12 +1,11 @@
 class Gigasecond
 
-	GIGASECOND = 1000000000
-
+	GIGASECOND = (10 ** 9).freeze
 	def self.from(birthdate)
 		birthdate + GIGASECOND
 	end
 end
 
 module BookKeeping
-	VERSION = 6
+	VERSION = 6.freeze
 end
